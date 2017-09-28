041312697e4dcf7fc7ae33068d48a841cfee57e4
nctvd123 <30915818+nctvd123@users.noreply.github.com>
2017-09-28 09:34:01 +0700

    Update nginx_php.sh

--- nginx_php.sh
+++ nginx_php.sh
@@ -144,6 +144,7 @@ fi
         	#Tao file index de test
         	touch $document_root/index.php
 		touch $document_root/index.html
+		echo "toandaica" >> $document_root/index.html
         	echo "<?php phpinfo(); ?>" >> $document_root/index.php
 		$source/php/sbin/php-fpm
 		IF=`route | grep default | awk '{print $8}'`
