9c6be82af0015682e3ee90eb780aec4e6ce6b9f2
nctvd123 <30915818+nctvd123@users.noreply.github.com>
2017-10-15 16:13:29 +0700

    Update shell_ANP.sh

--- shell_ANP.sh
+++ shell_ANP.sh
@@ -11,46 +11,26 @@ if [ ! -d $path_shell ];then
 fi
 while [ $n -eq 1 ]
 do
-	echo "CAI DAT APACHE"	
-	mv apache.sh $path_shell
-	cd $path_shell
-	apache $version_apache $version_apr $version_apr_util $source $domainname $port $document_root
-	break
+        sh apache.sh
+        break
 done
 while [ $n -eq 2 ]
-do
-        echo "CAI DAT NGINX"
-	mv nginx.sh $path_shell
-        cd $path_shell
-	chmod +x nginx.sh
-        ./nginx.sh
+do      
+        sh nginx.sh
         break
 done
 while [ $n -eq 3 ]
 do
-        echo "CAI DAT PHP-FPM"
-	mv php.sh $path_shell
-        cd $path_shell
-	chmod +x php.sh
-        ./php.sh
+        sh php.sh
         break
 done
 while [ $n -eq 4 ]
 do
-        echo "CAI DAT APACHE VA PHP-FPM"
-	mv apache_php.sh $path_shell
-        cd $path_shell
-	chmod +x apache_php.sh
-        ./apache_php.sh
+        sh apache_php.sh
         break
 done
 while [ $n -eq 5 ]
 do
-        echo "CAI DAT NGINX VA PHP-FPM"
-	mv nginx_php.sh $path_shell
-        cd $path_shell
-	chmod +x nginx_php.sh
-        ./nginx_php.sh
+        sh nginx_php.sh
         break
 done
-
