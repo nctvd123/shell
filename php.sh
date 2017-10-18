bc0d81481ec54c29fc0ad5b56bc9bf072fa56f44
nctvd123 <30915818+nctvd123@users.noreply.github.com>
2017-10-18 17:36:06 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,3 +1,5 @@
+php ()
+{
 #!/bin/bash
 #Cai dat php-fpm
 path_shell=/root/scripts/static
@@ -9,45 +11,45 @@ yum install gcc.x86_64 gcc-c++.x86_64 make.x86_64 wget.x86_64 libxml2-devel.x86_
 rpm -ivh "http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm"
 yum -y install libmcrypt-devel
 cd $path_down
-wget http://php.net/downloads.php
-grep "ChangeLog" downloads.php
-echo -n "MOI ANH BAN NHAP PHIEN BAN PHP MOI NHAT O TREN THEO DANG php-5.6.31:"; read version_php
-wget http://am1.php.net/get/$version_php.tar.gz/from/this/mirror
+link_php=`curl -L http://php.net/downloads.php | grep tar.gz | grep 'php-7.1'|grep -v "MD5\|PGP\|SHA1\|SHA256"| cut -d / -f 3`
+echo $link_php
+wget http://am1.php.net/get/$link_php/from/this/mirror
 if [ "$?" != 0 ] && [ -d $path_shell ];then
         echo "Error download php"
 else
-	echo -n "Nhap duong dan chua file cai dat php:";read php_source
-	mv mirror $version_php.tar.gz
-	tar xzvf $version_php.tar.gz
+	mv mirror $link_php
+    version_php=`tar -xvzf $link_php`
 	cd $version_php
-	./configure \--prefix=$php_source/php \--enable-fpm \--with-libdir=lib64 \--with-bz2 \--with-config-file-path=$php_source/php/etc \--with-config-file-scan-dir=$php_source/php/etc/php.d \--with-curl=$php_source/lib \--with-gd \--with-gettext \--with-jpeg-dir=$php_source/lib \--with-freetype-dir=$php_source/lib \--with-kerberos \--with-mcrypt \--with-mhash \--with-mysql \--with-mysqli \--with-pdo-mysql=shared \--with-pdo-sqlite=shared \--with-pear=$php_source/lib/php \--with-png-dir=$php_source/lib \--with-pspell \--with-sqlite=shared \--with-tidy \--with-xmlrpc \--with-xsl \--with-zlib \--with-zlib-dir=$duongdan/lib \--with-openssl \--with-iconv \--enable-bcmath \--enable-calendar \--enable-exif \--enable-ftp \--enable-gd-native-ttf \--enable-libxml \--enable-magic-quotes \--enable-soap \--enable-sockets \--enable-mbstring \--enable-zip \--enable-wddx
+	./configure \--prefix=$1/php \--enable-fpm \--with-libdir=lib64 \--with-bz2 \--with-config-file-path=$1/php/etc \--with-config-file-scan-dir=$1/php/etc/php.d \--with-curl=$1/lib \--with-gd \--with-gettext \--with-jpeg-dir=$1/lib \--with-freetype-dir=$1/lib \--with-kerberos \--with-mcrypt \--with-mhash \--with-mysql \--with-mysqli \--with-pdo-mysql=shared \--with-pdo-sqlite=shared \--with-pear=$1/lib/php \--with-png-dir=$1/lib \--with-pspell \--with-sqlite=shared \--with-tidy \--with-xmlrpc \--with-xsl \--with-zlib \--with-zlib-dir=$duongdan/lib \--with-openssl \--with-iconv \--enable-bcmath \--enable-calendar \--enable-exif \--enable-ftp \--enable-gd-native-ttf \--enable-libxml \--enable-magic-quotes \--enable-soap \--enable-sockets \--enable-mbstring \--enable-zip \--enable-wddx
 	make -j 2
 	make install
 	n=`echo $version_php | cut -d . -f 1`
 	if  [ $n == php-5 ]; then
-		cp php.ini-production $php_source/php/etc/php.ini
-		cd $php_source/php/etc/
+		cp php.ini-production $1/php/etc/php.ini
+		cd $1/php/etc/
 		cp php-fpm.conf.default php-fpm.conf
-		sed -i -e "s/;pid = /pid = /" $php_source/php/etc/php-fpm.conf
-		sed -i -e "s/;error_log = /error_log = /" $php_source/php/etc/php-fpm.conf
-		sed -i -e "s/user = nobody/user = nginx/" $php_source/php/etc/php-fpm.conf
-		sed -i -e "s/group = nobody/group = nginx/" $php_source/php/etc/php-fpm.conf
-		sed -i -e "s/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 127.0.0.1/" $php_source/php/etc/php-fpm.conf
+		sed -i -e "s/;pid = /pid = /" $1/php/etc/php-fpm.conf
+		sed -i -e "s/;error_log = /error_log = /" $1/php/etc/php-fpm.conf
+		sed -i -e "s/user = nobody/user = nginx/" $1/php/etc/php-fpm.conf
+		sed -i -e "s/group = nobody/group = nginx/" $1/php/etc/php-fpm.conf
+		sed -i -e "s/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 127.0.0.1/" $1/php/etc/php-fpm.conf
 	else
-		cp php.ini-production $php_source/php/etc/php.ini
-        	cd $php_source/php/etc/
+		cp php.ini-production $1/php/etc/php.ini
+        	cd $1/php/etc/
         	cp php-fpm.conf.default php-fpm.conf
-        	sed -i -e "s/;pid = /pid = /" $php_source/php/etc/php-fpm.conf
-        	sed -i -e "s/;error_log = /error_log = /" $php_source/php/etc/php-fpm.conf
-		cp $php_source/php/etc/php-fpm.d/www.conf.default $php_source/php/etc/php-fpm.d/www.conf
-        	sed -i -e "s/user = nobody/user = nginx/" $php_source/php/etc/php-fpm.d/www.conf
-        	sed -i -e "s/group = nobody/group = nginx/" $php_source/php/etc/php-fpm.d/www.conf
-        	sed -i -e "s/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 127.0.0.1/" $php_source/php/etc/php-fpm.d/www.conf
+        	sed -i -e "s/;pid = /pid = /" $1/php/etc/php-fpm.conf
+        	sed -i -e "s/;error_log = /error_log = /" $1/php/etc/php-fpm.conf
+		cp $1/php/etc/php-fpm.d/www.conf.default $1/php/etc/php-fpm.d/www.conf
+        	sed -i -e "s/user = nobody/user = nginx/" $1/php/etc/php-fpm.d/www.conf
+        	sed -i -e "s/group = nobody/group = nginx/" $1/php/etc/php-fpm.d/www.conf
+        	sed -i -e "s/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 127.0.0.1/" $1/php/etc/php-fpm.d/www.conf
 	fi
 fi
 #chmod +x /etc/init.d/php-fpm
 #chkconfig php-fpm on
 #service php-fpm restart
-$php_source/php/sbin/php-fpm
+$1/php/sbin/php-fpm
 netstat -ntpl
 echo "DONE"
+}
+php /usr/local
