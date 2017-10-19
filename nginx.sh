13d8231eab38c8c4b19058a6e75bf98568f6aef0
nctvd123 <30915818+nctvd123@users.noreply.github.com>
2017-10-19 14:42:57 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,4 @@
-nginx () 
+nginx_php () 
 {
 #!/bin/bash
 #Cai dat thoi gian
@@ -77,7 +77,7 @@ fi
 		echo "    root           $4;" >> $1/nginx/conf/conf.d/$2.conf
 		echo "    fastcgi_pass   127.0.0.1:9000;" >> $1/nginx/conf/conf.d/$2.conf
 		echo "    fastcgi_index  index.php;" >> $1/nginx/conf/conf.d/$2.conf
-		echo "    fastcgi_param  SCRIPT_FILENAME    $4/'$fastcgi_script_name';" >> $1/nginx/conf/conf.d/$2.conf
+		echo "    fastcgi_param  SCRIPT_FILENAME  $4/$fastcgi_script_name;" >> $1/nginx/conf/conf.d/$2.conf
 		echo "    include        fastcgi_params;" >> $1/nginx/conf/conf.d/$2.conf
 		echo "    }" >> $1/nginx/conf/conf.d/$2.conf
 		echo "        error_page 404 /404.html;" >> $1/nginx/conf/conf.d/$2.conf
@@ -87,45 +87,6 @@ fi
 		echo "        location = /50x.html {" >> $1/nginx/conf/conf.d/$2.conf
 		echo "    }" >> $1/nginx/conf/conf.d/$2.conf
 		echo "}" >> $1/nginx/conf/conf.d/$2.conf
-		#Cai dat php-fpm
-		yum -y install gcc.x86_64 gcc-c++.x86_64 make.x86_64 wget.x86_64 libxml2-devel.x86_64 openssl-devel.x86_64 pcre-devel.x86_64 libjpeg-devel curl-devel curl-devel.x86_64 libjpeg-devel.x86_64 libpng-devel.x86_64 freetype-devel.x86_64 libmcrypt.x86_64 libmcrypt-devel.x86_64  mhash.x86_64 mhash-devel.x86_64 php-mysql.x86_64 mysql-devel.x86_64 bzip2-devel.x86_64 aspell-devel.x86_64 libtidy.x86_64 libtidy-devel.x86_64 libxslt.x86_64 libxslt-devel.x86_64 glibc-utils.x86_64 libjpeg-turbo8-dev libpng-devel libmcrypt-devel.x86_64 libtool-ltdl-devel.x86_64
-		rpm -ivh "http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm"
-		yum -y install libmcrypt-devel
-		cd $path_down
-		link_php=`curl -L http://php.net/downloads.php | grep tar.gz | grep 'php-7.1'|grep -v "MD5\|PGP\|SHA1\|SHA256"| cut -d / -f 3`
-		echo $link_php
-		wget http://am1.php.net/get/$link_php/from/this/mirror
-		if [ "$?" != 0 ] && [ -d $path_shell ];then
-        		echo "Error download php"
-		else
-        		mv mirror $link_php
-     			version_php=`tar -xvzf $link_php`
-        		cd $version_php
-        		./configure \--prefix=$1/php \--enable-fpm \--with-libdir=lib64 \--with-bz2 \--with-config-file-path=$1/php/etc \--with-config-file-scan-dir=$1/php/etc/php.d \--with-curl=$1/lib \--with-gd \--with-gettext \--with-jpeg-dir=$1/lib \--with-freetype-dir=$1/lib \--with-kerberos \--with-mcrypt \--with-mhash \--with-mysql \--with-mysqli \--with-pdo-mysql=shared \--with-pdo-sqlite=shared \--with-pear=$1/lib/php \--with-png-dir=$1/lib \--with-pspell \--with-sqlite=shared \--with-tidy \--with-xmlrpc \--with-xsl \--with-zlib \--with-zlib-dir=$1/lib \--with-openssl \--with-iconv \--enable-bcmath \--enable-calendar \--enable-exif \--enable-ftp \--enable-gd-native-ttf \--enable-libxml \--enable-magic-quotes \--enable-soap \--enable-sockets \--enable-mbstring \--enable-zip \--enable-wddx
-        		make -j 2
-        		make install
-			n=`echo $version_php | cut -d . -f 1`
-        		if  [ $n == php-5 ]; then
-                		cp php.ini-production $1/php/etc/php.ini
-                		cd $1/php/etc/
-                		cp php-fpm.conf.default php-fpm.conf
-                		sed -i -e "s/;pid = /pid = /" $1/php/etc/php-fpm.conf
-                		sed -i -e "s/;error_log = /error_log = /" $1/php/etc/php-fpm.conf
-                		sed -i -e "s/user = nobody/user = nginx/" $1/php/etc/php-fpm.conf
-                		sed -i -e "s/group = nobody/group = nginx/" $1/php/etc/php-fpm.conf
-                		sed -i -e "s/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 127.0.0.1/" $1/php/etc/php-fpm.conf
-        		else
-                		cp php.ini-production $1/php/etc/php.ini
-                		cd $1/php/etc/
-                		cp php-fpm.conf.default php-fpm.conf
-                		sed -i -e "s/;pid = /pid = /" $1/php/etc/php-fpm.conf
-                		sed -i -e "s/;error_log = /error_log = /" $1/php/etc/php-fpm.conf
-                		cp $1/php/etc/php-fpm.d/www.conf.default $1/php/etc/php-fpm.d/www.conf
-                		sed -i -e "s/user = nobody/user = nginx/" $1/php/etc/php-fpm.d/www.conf
-                		sed -i -e "s/group = nobody/group = nginx/" $1/php/etc/php-fpm.d/www.conf
-                		sed -i -e "s/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 127.0.0.1/" $1/php/etc/php-fpm.d/www.conf
-        		fi
-		fi
 		#cai dat ssl
 		mkdir -p $1/ssl
 		cd $1/ssl
@@ -133,14 +94,11 @@ fi
 		#Tao duong dan document root
  		mkdir -p $4
 		chown -R nginx:nginx $4
-        netstat -ntpl
         #Tao file index de test
         touch $4/index.php
-		touch $4/index.html
 		echo "toandaica" >> $4/index.html
-        echo "<?php phpinfo(); ?>" >> $4/index.php
 		$1/nginx/sbin/nginx
-		$1/php/sbin/php-fpm
+		netstat -ntpl
 		IF=`route | grep default | awk '{print $8}'`
         ip=`ip a | grep $IF | grep inet | awk '{print $2}' | cut -d / -f 1`
         links $ip:$3/index.php
